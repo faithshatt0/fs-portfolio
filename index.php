4d526bf99a31089d4d31f31075fc016eb363ee00
faithshatt0 <faith.shatto@gmail.com>
2019-04-19 14:25:38 -0700

    Initial website setup

---
+++ index.php
@@ -0,0 +1,20 @@
+// index.php
+// Author: Faith Shatto
+// Description: Initial project start-up starts here
+<!doctype html>
+
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Web Leaf Design</title>
+    <meta name="description" content="Web Leaf Design">
+    <meta name="author" content="Faith Shatto">
+    <link rel="stylesheet" href="css/styles.css?v=1.0">
+
+  </head>
+
+  <body>
+    <!--<script src="js/scripts.js"></script>-->
+  </body>
+
+</html>
