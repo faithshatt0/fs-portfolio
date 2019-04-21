7faa27079d6ca9524147afd4de67c990253644b5
faithshatt0 <faith.shatto@gmail.com>
2019-04-20 18:16:46 -0700

    Added image

--- index.php
+++ index.php
@@ -1,8 +1,3 @@
-// index.php
-// Author: Faith Shatto
-// Description: Initial project start-up starts here
-<!doctype html>
-
 <html lang="en">
   <head>
     <meta charset="utf-8">
@@ -10,11 +5,14 @@
     <meta name="description" content="Web Leaf Design">
     <meta name="author" content="Faith Shatto">
     <link rel="stylesheet" href="css/styles.css?v=1.0">
-
   </head>
 
   <body>
-    <!--<script src="js/scripts.js"></script>-->
+    <div id="header-container">
+      <h1>Web Leaf</h1>
+    </div>
+    <img id="painted-image" src="slashed.png" />
+
   </body>
 
 </html>
